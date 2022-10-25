0cb4f1949aa502b28db50335f8e46237536e8e59
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-10-25 14:11:28 +0200

    Create generic theme && global styles to entiry app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Theme from '../Themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Theme>
+    <Component {...pageProps} />
+  </Theme>
+  )
 }
 
 export default MyApp
