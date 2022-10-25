af257ef963e9a155a5550edac4e517f41b6f73d4
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-10-25 14:13:07 +0200

    Edit typo styles

--- Themes/global.js
+++ Themes/global.js
@@ -15,8 +15,8 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: ${props => props.theme.fonts.main};
     font-size: 1.6rem;
-    background: ${props => props.theme.colors.background1};
-    color: ${props => props.theme.colors.primary1};
+    background: ${props => props.theme.background.mainBackground};
+    color: ${props => props.theme.colors.primary};
     cursor: default;
   }
   h1,h2,h3,h4,h5,h6,button {
